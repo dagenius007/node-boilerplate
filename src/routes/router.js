41f28008b400d00dce382cd7b4116b6fba70e6c1
Joshua Oluikpe <joshua.o@softcom.ng>
2020-02-02 17:45:25 +0100

    Home route

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,6 +4,10 @@ const User = require('../models/User');
 
 const router = new Router();
 
+router.get('/', function(req, res) {
+	console.log('ji');
+	res.status(200).json({ message: 'Hello' });
+});
 router.post('/signup', UserController.signup);
 router.post('/login', UserController.loginUser);
 
