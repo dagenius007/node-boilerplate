46c569390970127b2b26a0f5b56c04da94a78ede
Joshua Oluikpe <joshua.o@softcom.ng>
2020-02-03 07:46:30 +0100

    Path referencing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import config from './config';
 import mongoose from './services/mongo';
 import express from './services/express';
 
-
-
 const app = express(config.apiRoot);
 
-global.__basedir = __dirname;
+// global.__basedir = __dirname;
 
 mongoose.connect(config.mongo.uri, { useCreateIndex: true, useNewUrlParser: true });
 // mongoose.Promise = Promised
